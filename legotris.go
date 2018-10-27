51dc49e6de9eb447ed78cf2617f214fa687d2b28
Aga <mishyag@gmail.com>
2018-10-27 20:22:14 +0200

    zaladowanie i wyswietlenie obrazka

--- legotris.go
+++ legotris.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"image"
+	_ "image/png"
+	"os"
+
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
@@ -8,7 +12,7 @@ import (
 
 func run() {
 	cfg := pixelgl.WindowConfig{
-		Title:  "Pixel Rocks!",
+		Title:  "LEGO TETRIS",
 		Bounds: pixel.R(0, 0, 1024, 768),
 		VSync:  true,
 	}
@@ -17,11 +21,35 @@ func run() {
 		panic(err)
 	}
 
-	win.Clear(colornames.Lightsteelblue)
+	pic, err := loadPicture("brickBlack.png")
+	if err != nil {
+		panic(err)
+	}
+
+	sprite := pixel.NewSprite(pic, pic.Bounds()) //obiekt sluzacy do wyswietlenia obrazka
+
+	win.Clear(colornames.Lightsteelblue) //wyczyszczenie okna przed wyswietleniem
+
+	sprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
 
 	for !win.Closed() {
-		win.Update()
+
+		win.Update() // odswiezenie okna
 	}
+
+}
+
+func loadPicture(path string) (pixel.Picture, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close() // defer = finnaly
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+	return pixel.PictureDataFromImage(img), nil
 }
 
 func main() {
